c8dd4ad56eb9e8999e9b94450b39a53ceddb99c0
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-03-14 13:50:27 +0300

    Initial Commit

---
+++ TestFunc/TestFuncApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFuncApp.swift
+//  TestFunc
+//
+//  Created by Marat Fakhrizhanov on 14.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestFuncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
