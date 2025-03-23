4f1708d5e03da063c1322d75665a39e489691c9f
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-03-23 20:07:47 +0300

    //

--- TestFunc/ContentView.swift
+++ TestFunc/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Image(systemName: "globe")  //
+                .imageScale(.large) //
             Text("Hello, world!")
         }
         .padding()
